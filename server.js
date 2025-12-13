3f6d9d7f296595005138b16f16b639f49aedc313
Nicolas Perez <nicolas.perez.mt@usa.edu.co>
2025-12-13 17:52:37 -0500

    Antes de la primera prueba4

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
+import "dotenv/config";
 import express from "express";
-import dotenv from "dotenv";
 import webhookRouter from "./routes/webhook.js";
-
-dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use("/webhook", webhookRouter);
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+app.listen(PORT, () =>
+    console.log(`🚀 Servidor corriendo en puerto ${PORT}`)
+);
