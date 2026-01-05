c20e152be484b446580dafe434bb7e11987e2f63
Nicolas Perez <nicolas.perez.mt@usa.edu.co>
2026-01-05 16:14:54 -0500

    fix estado pedidos 16

--- db/actualizarEstadoPedido.js
+++ db/actualizarEstadoPedido.js
@@ -1,19 +1,22 @@
 import { pool } from "./init.js";
 
 export async function actualizarEstadoPedido(orderCode, nuevoEstado) {
+    const fueEntregado = nuevoEstado === "ENTREGADO";
+
     const { rows, rowCount } = await pool.query(
         `
     UPDATE orders
-    SET estado_pedido = $1,
-        fue_entregado = CASE 
-          WHEN $1 = 'ENTREGADO' THEN true 
-          ELSE fue_entregado 
-        END
+    SET
+      estado_pedido = $1,
+      fue_entregado = CASE
+        WHEN $3 = true THEN true
+        ELSE fue_entregado
+      END
     WHERE order_code = $2
       AND cancelado = false
     RETURNING *
     `,
-        [nuevoEstado, orderCode]
+        [nuevoEstado, orderCode, fueEntregado]
     );
 
     if (rowCount === 0) return null;
